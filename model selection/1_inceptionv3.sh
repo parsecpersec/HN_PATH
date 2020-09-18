cd7cb28222db496ae611511f209e2e1fdbdf4b44
parsecpersec <53437339+parsecpersec@users.noreply.github.com>
2020-09-18 20:29:43 +0800

    Update 1_inceptionv3.sh

--- model selection/1_inceptionv3.sh
+++ model selection/1_inceptionv3.sh
@@ -1,41 +1,56 @@
-sudo mount /dev/sdb2 /run/media      # wait
+python3 /home/xuhao/pjk/codes/DeepPATH_code/00_preprocessing/0b_tileLoop_deepzoom4.py  -s 224 -e 0 -j 32 -B 50 -M 10 -o /run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/10x "/run/media/xuhao/data/HNSC_slides/*/*svs"
+python3 /home/xuhao/pjk/codes/DeepPATH_code/00_preprocessing/0b_tileLoop_deepzoom4.py  -s 224 -e 0 -j 32 -B 50 -M 40 -o /run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/40x "/run/media/xuhao/data/HNSC_slides/*/*svs"
 
-python3 /home/xuhao/pjk/codes/DeepPATH_code/00_preprocessing/0b_tileLoop_deepzoom4.py  -s 224 -e 0 -j 32 -B 50 -M 10 -o /run/media/slide/1_inceptionv3/10x "/run/media/slide/HNSCslides/*/*svs"
-python3 /home/xuhao/pjk/codes/DeepPATH_code/00_preprocessing/0b_tileLoop_deepzoom4.py  -s 224 -e 0 -j 32 -B 50 -M 40 -o /run/media/slide/1_inceptionv3/40x "/run/media/slide/HNSCslides/*/*svs"
+cd sorted_10x
 
-cd sorted
+python3 /home/xuhao/pjk/codes/DeepPATH_code/00_preprocessing/0d_SortTiles.py --SourceFolder='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/10x' --Magnification=10.0  --MagDiffAllowed=0 --SortingOption=2  --PatientID=12 --nSplit 0 --JsonFile='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/prog.json' --PercentTest=15 --PercentValid=15
 
-python3 /home/xuhao/pjk/codes/DeepPATH_code/00_preprocessing/0d_SortTiles.py --SourceFolder='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/HNSC_results/224px_Tiled' --Magnification=10.0  --MagDiffAllowed=0 --SortingOption=2  --PatientID=12 --nSplit 0 --JsonFile='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/HNSC_results/no_missing.json' --PercentTest=15 --PercentValid=15
-python3 /home/xuhao/pjk/codes/DeepPATH_code/00_preprocessing/0d_SortTiles.py --SourceFolder='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/HNSC_results/224px_Tiled' --Magnification=40.0  --MagDiffAllowed=0 --SortingOption=2  --PatientID=12 --nSplit 0 --JsonFile='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/HNSC_results/no_missing.json' --PercentTest=15 --PercentValid=15
+cd sorted_40x
+
+python3 /home/xuhao/pjk/codes/DeepPATH_code/00_preprocessing/0d_SortTiles.py --SourceFolder='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/40x' --Magnification=40.0  --MagDiffAllowed=0 --SortingOption=2  --PatientID=12 --nSplit 0 --JsonFile='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/prog.json' --PercentTest=15 --PercentValid=15
 
 cd ..
 mkdir r1_TFRecord_test
 mkdir r1_TFRecord_valid
 mkdir r1_TFRecord_train
 
-python3 /home/xuhao/pjk/codes/DeepPATH_code/00_preprocessing/TFRecord_2or3_Classes/build_TF_test.py --directory='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/HNSC_results/sorted'  --output_directory='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/HNSC_results/r1_TFRecord_test' --num_threads=2 --one_FT_per_Tile=False --ImageSet_basename='test'
+python3 /home/xuhao/pjk/codes/DeepPATH_code/00_preprocessing/TFRecord_2or3_Classes/build_TF_test.py --directory='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/sorted_10x'  --output_directory='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/r1_TFRecord_test' --num_threads=2 --one_FT_per_Tile=False --ImageSet_basename='test'
 
-python3 /home/xuhao/pjk/codes/DeepPATH_code/00_preprocessing/TFRecord_2or3_Classes/build_TF_test.py --directory='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/HNSC_results/sorted'  --output_directory='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/HNSC_results/r1_TFRecord_valid' --num_threads=2 --one_FT_per_Tile=False --ImageSet_basename='valid'
+python3 /home/xuhao/pjk/codes/DeepPATH_code/00_preprocessing/TFRecord_2or3_Classes/build_TF_test.py --directory='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/sorted_10x'  --output_directory='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/r1_TFRecord_valid' --num_threads=2 --one_FT_per_Tile=False --ImageSet_basename='valid'
 
-python3 /home/xuhao/pjk/codes/DeepPATH_code/00_preprocessing/TFRecord_2or3_Classes/build_image_data.py --directory='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/HNSC_results/sorted' --output_directory='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/HNSC_results/r1_TFRecord_train' --train_shards=1024  --validation_shards=128 --num_threads=2
+python3 /home/xuhao/pjk/codes/DeepPATH_code/00_preprocessing/TFRecord_2or3_Classes/build_image_data.py --directory='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/sorted_10x' --output_directory='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/r1_TFRecord_train' --train_shards=1024  --validation_shards=128 --num_threads=2
 
 mkdir r1_results
 cd /home/xuhao/pjk/codes/DeepPATH_code/01_training/xClasses
-python3 -m inception.imagenet_train --num_gpus=4 --batch_size=200 --train_dir=/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/HNSC_results/r1_results --data_dir=/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/HNSC_results/r1_TFRecord_train --ClassNumber=2 --model='0_softmax' --max_steps=100001
+python3 -m inception.imagenet_train --num_gpus=4 --batch_size=200 --train_dir=/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/r1_results --data_dir=/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/r1_TFRecord_train --ClassNumber=2 --model='0_softmax' --max_steps=40001
 
-cd /run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/HNSC_results
+cd ..
+mkdir r4_TFRecord_test
+mkdir r4_TFRecord_valid
+mkdir r4_TFRecord_train
+
+python3 /home/xuhao/pjk/codes/DeepPATH_code/00_preprocessing/TFRecord_2or3_Classes/build_TF_test.py --directory='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/sorted_40x'  --output_directory='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/r4_TFRecord_test' --num_threads=2 --one_FT_per_Tile=False --ImageSet_basename='test'
+
+python3 /home/xuhao/pjk/codes/DeepPATH_code/00_preprocessing/TFRecord_2or3_Classes/build_TF_test.py --directory='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/sorted_40x'  --output_directory='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/r4_TFRecord_valid' --num_threads=2 --one_FT_per_Tile=False --ImageSet_basename='valid'
+
+python3 /home/xuhao/pjk/codes/DeepPATH_code/00_preprocessing/TFRecord_2or3_Classes/build_image_data.py --directory='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/sorted_40x' --output_directory='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/r4_TFRecord_train' --train_shards=1024  --validation_shards=128 --num_threads=2
+
+mkdir r4_results
+cd /home/xuhao/pjk/codes/DeepPATH_code/01_training/xClasses
+python3 -m inception.imagenet_train --num_gpus=4 --batch_size=200 --train_dir=/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/r4_results --data_dir=/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/r4_TFRecord_train --ClassNumber=2 --model='0_softmax' --max_steps=140001
+
+cd /run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3
 
 #### valid ####
 mkdir r1_valid
-export CHECKPOINT_PATH='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/HNSC_results/r1_results'
-export OUTPUT_DIR='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/HNSC_results/r1_valid'
-export DATA_DIR='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/HNSC_results/r1_TFRecord_valid'
-export LABEL_FILE='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/HNSC_results/labelref_r1.txt'
+sudo chmod 777 /run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/r1_valid/
+export CHECKPOINT_PATH='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/r1_results'
+export OUTPUT_DIR='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/r1_valid'
+export DATA_DIR='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/r1_TFRecord_valid'
+export LABEL_FILE='/run/media/xuhao/4bf9cc2e-2b9c-4829-a072-c0933c6ecbd8/slide/1_inceptionv3/labelref.txt'
 
 declare -i count=5000
 declare -i step=5000
-declare -i NbClasses=2
-sudo mkdir -p $OUTPUT_DIR/tmp_checkpoints
 declare -i NbClasses=2
 sudo mkdir -p $OUTPUT_DIR/tmp_checkpoints
 export CUR_CHECKPOINT=$OUTPUT_DIR/tmp_checkpoints
